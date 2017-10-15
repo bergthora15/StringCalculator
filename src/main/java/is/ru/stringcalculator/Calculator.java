eee1183fca20718d40a6487651f382491cb23df9
Begga <bergthora15@ru.is>
2017-10-15 10:41:09 +0000

    refactoring code for testOneNumber

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,6 +6,20 @@ public class Calculator {
 		if(numbers.isEmpty()) {
 			return 0;	
 		}
-		return 1;
+		else{
+			if(numbers.contains(",")) {
+				String numb[] = numbers.split(",");
+				int total = 0;
+				for (String n : numb) {	
+					total +=toInt(n);
+				}
+				return total;
+			}
+			return 1;
+		}
+		
+	}
+	private static int toInt(String numbers) {
+		return Integer.parseInt(numbers);
 	}
 }
